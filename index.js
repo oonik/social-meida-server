15f3e755ac461f4d67eb5739ed11fd930da732ae
oonik <aniksarkar0177@gmail.com>
2023-10-13 11:37:55 +0600

    complete server side

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -23,12 +23,55 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const postCollection = client.db("socialMedia").collection("post");
+    const userCollection = client.db("socialMedia").collection("users");
 
     app.get('/post', async(req, res)=> {
      const query = {};
-     const result = await postCollection.find(query).toArray();
+     const result = (await postCollection.find(query).toArray()).reverse();
      res.send(result);
+    });
+
+    app.get('/users', async(req, res)=> {
+     const query = {};
+     const result = await userCollection.find(query).toArray();
+     res.send(result);
+    });
+
+    app.get('/post/:id', async(req, res)=> {
+     const id = req.params.id;
+     const query = {_id: new ObjectId(id)};
+     const result = await postCollection.findOne(query);
+     res.send(result);   
+    });
+
+    app.post('/users', async(req, res)=> {
+     const user = req.body;
+     const result = await userCollection.insertOne(user);
+     res.send(result);   
+    });
+
+    app.put('/users/:id', async(req, res)=> {
+      const id = req.params.id;
+      const user = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: user.name,
+          university: user.university,
+          email: user.email,
+          address: user.address 
+        },
+      }
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
     })
+
+    app.post('/post', async(req, res)=> {
+     const post = req.body;
+     const result = await postCollection.insertOne(post);
+     res.send(result);   
+    });
    
   } finally {
     
